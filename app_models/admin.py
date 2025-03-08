7899f41800345ccce73f6b78c00ec1c8436e9d43
Edomondo <edghimakoll@gmail.com>
2025-03-08 04:40:20 +0100

    feat(admin): adding action method to approve or reject user verification

--- app_models/admin.py
+++ app_models/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from app_models.models import (
     ServiceRequest,
@@ -34,7 +35,30 @@ class ServiceProposalAdmin(admin.ModelAdmin):
 
 @admin.register(UserVerification)
 class UserVerificationAdmin(admin.ModelAdmin):
-    list_display = ("user", "verification_photo", "created_at", "updated_at")
+    list_display = ("user", "verification_photo", "created_at", "updated_at", "approved")
+    
+    actions = ["approve_verification", "reject_verification"]
+    
+    def approved(self, obj) -> bool:
+        "Returns a check mark if the user is verified, otherwise a cross mark"
+        if obj.user.is_verified:
+            return format_html('<span style="color: green;">✔️</span>')
+        else:
+            return format_html('<span style="color: red;">❌</span>')
+    
+    @admin.action(description="Approve selected verifications")
+    def approve_verification(self, request, queryset):
+        for verification in queryset:
+            verification.user.is_verified = True
+            verification.user.is_active = True
+            verification.user.save()
+    
+    @admin.action(description="Reject selected verifications")
+    def reject_verification(self, request, queryset):
+        for verification in queryset:
+            verification.user.is_verified = False
+            verification.user.is_active = False
+            verification.user.save()
 
 
 admin.site.site_header = "X-Project Administration Site"
