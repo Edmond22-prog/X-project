9d5797fd81d83e0f85107341d7fcbdca30f37075
Edomondo <edghimakoll@gmail.com>
2025-03-08 14:22:26 +0100

    refactor(settings): allow access from any platform

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7d3-+or0l!an*5js2(gj!w(gyq6w(fl_6mfp*a!dzt1^0q)9&k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
